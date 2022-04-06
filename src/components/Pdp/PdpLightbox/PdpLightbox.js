eb4d089e491a848c8ec2062d65a4f0037e97a1fb
Roksana <roksana.rebiewska@gmail.com>
2022-04-06 13:16:26 +0200

    Move lightbox to Portal

--- src/components/Pdp/PdpLightbox/PdpLightbox.js
+++ src/components/Pdp/PdpLightbox/PdpLightbox.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
 import classes from './PdpLightbox.module.scss';
 
@@ -28,41 +29,46 @@ const PdpLightbox = React.forwardRef(
 
     return (
       <>
-        <div
-          className={classes['pdp-lightbox-background']}
-          onClick={lightboxToggle}
-        ></div>
-        <div className={classes['pdp-lightbox-div']}>
-          <img
-            src={closeIcon}
-            id={classes['close-icon']}
-            onClick={lightboxToggle}
-            alt="close"
-          />
-          <img
-            src={ProductOne}
-            id={classes['main-lightbox']}
-            ref={mainLighboxRef}
-          />
-          <div className={classes['pdp-lightbox-teasers']}>
-            {productImages.map((image, index) => (
+        {ReactDOM.createPortal(
+          <>
+            <div
+              className={classes['pdp-lightbox-backdrop']}
+              onClick={lightboxToggle}
+            ></div>
+            <div className={classes['pdp-lightbox-div']}>
               <img
-                src={image.teaser}
-                data-src={image.full}
-                id={`${image.id}-lightbox`}
-                key={image.key}
-                onClick={lightboxChangeHandler}
-                ref={(element) => (imageRef.current[index] = element)}
+                src={closeIcon}
+                id={classes['close-icon']}
+                onClick={lightboxToggle}
+                alt="close"
               />
-            ))}
-          </div>
-          <button id={classes.previous} onClick={previousHandler}>
-            <img src={previousIcon} />
-          </button>
-          <button id={classes.next} onClick={nextHandler}>
-            <img src={nextIcon} />
-          </button>
-        </div>
+              <img
+                src={ProductOne}
+                id={classes['main-lightbox']}
+                ref={mainLighboxRef}
+              />
+              <div className={classes['pdp-lightbox-teasers']}>
+                {productImages.map((image, index) => (
+                  <img
+                    src={image.teaser}
+                    data-src={image.full}
+                    id={`${image.id}-lightbox`}
+                    key={image.key}
+                    onClick={lightboxChangeHandler}
+                    ref={(element) => (imageRef.current[index] = element)}
+                  />
+                ))}
+              </div>
+              <button id={classes.previous} onClick={previousHandler}>
+                <img src={previousIcon} />
+              </button>
+              <button id={classes.next} onClick={nextHandler}>
+                <img src={nextIcon} />
+              </button>
+            </div>
+          </>,
+          document.getElementById('lightbox-root')
+        )}
       </>
     );
   }
